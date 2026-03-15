28e933a2f9d267a9f18d325c3ebe033b14875881
GuilhermeAmeixaF <guilhermeameixafreitas@gmail.com>
2026-03-15 15:50:47 -0300

    Corrigindo erro na criação do Bando de dados.

---
+++ Migrations/20260314181821_CriarTabelaProdutos.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyCrudApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriarTabelaProdutos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", nullable: false),
+                    Preco = table.Column<decimal>(type: "TEXT", nullable: false),
+                    QuantidadeEmEstoque = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produtos");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,20 +1,22 @@
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using MyCrudApi.Models;
 using Microsoft.EntityFrameworkCore;
-using System.Numerics;
+
+
 
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Configuracao do DbContext com SQLite
-builder.Services.AddDbContext<AppDbContext>(options =>
-        options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Adiciona servicos ao container
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Configuracao do DbContext com SQLite
+var dbPath = Path.Combine(builder.Environment.ContentRootPath, "produtos.db");
+
+builder.Services.AddDbContext<AppDbContext>(options => 
+options.UseSqlite($"Data Source={dbPath}"));
 
 // CONGURACAO do CORS
 builder.Services.AddCors(options =>
@@ -30,6 +32,52 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+// PRIMEIRO BLOCO: CRIAR BANCO E TABELAS apos criar fecha a conexao
+using (var scope  =  app.Services.CreateScope())
+{
+        var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+        // GARANTE QUE O BANCO E AS TABELAS EXISTEM
+       // dbContext.Database.EnsureDeleted(); //opcional: apaga banco existente
+        
+       dbContext.Database.EnsureCreated(); // Cria banco e tabelas se nao existirem
+
+
+        // FECHA A CONEXAO EXPLICITAMENTE
+        Console.WriteLine("✅ Banco de dados e tabelas criados com sucesso!");
+}
+
+// SEGUNDO BLOCO: Cria um novo contexto para adicionar dados
+using (var scope = app.Services.CreateScope())
+{
+        var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+        dbContext.Database.EnsureCreated();
+
+         try
+                {
+                         dbContext.Produtos.AddRange(
+                                        new Produto{Nome = "Mouse Gamer", Preco = 89.90m, QuantidadeEmEstoque = 15},
+                                        new Produto{Nome = "Teclado mecânico", Preco = 199.99m, QuantidadeEmEstoque = 8},
+                                        new Produto{Nome = "Monitor 24\"", Preco = 899.99m, QuantidadeEmEstoque = 4 }
+                        
+                                );
+                        
+                        dbContext.SaveChanges();
+                        Console.WriteLine($"✅ {dbContext.Produtos.Count()} produtos inseridos com sucesso!");
+
+                                
+                }
+
+
+                catch (Exception ex)
+                {
+                        Console.WriteLine($"❌ Erro detalhado: {ex.Message}");
+
+                }       
+        
+}
+
 // Configurar o pipeline de requisicoes HTTP
 if (app.Environment.IsDevelopment())
 {
@@ -37,24 +85,32 @@ if (app.Environment.IsDevelopment())
         app.UseSwaggerUI();
 }
 
-else
-{
-    app.UseHttpsRedirection(); // so em producao
-}
+//else {app.UseHttpsRedirection(); // so em producao}
 // Arquivos estaticos
+
+app.UseCors("PermitirTudo");
+app.UseAuthorization();
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
-app.UseCors("PermitirTudo");
-app.UseAuthorization();
+
 app.MapControllers();
 
-// Cria o banco de dados if not exist
+// ultma  verifiaçãoo antes de rodas
 using (var scope = app.Services.CreateScope())
 {
-        var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-        dbContext.Database.EnsureCreated();
+        try
+        {
+                var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                var count = dbContext.Produtos.Count();
+                Console.WriteLine($"✅ Banco OK! {count} produtos encontrados");
+        }
+
+        catch( Exception ex)
+        {
+                Console.WriteLine($"⚠️ Aviso: {ex.Message}");
+        }
 }
 
 app.Run();
